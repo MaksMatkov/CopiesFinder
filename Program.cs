97ac1e67bba5894794cde6534fb5e7c2cfe4d68a
Maks Matkov <maxmatkov@gmail.com>
2022-09-07 13:50:15 +0300

    Start 2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task1
@@ -17,6 +18,10 @@ namespace Task1
         
         static void Main(string[] args)
         {
+
+
+            Start2();
+            return;
 
             Task thread = Task.Factory.StartNew(() => {
                 while(!IsDone) { }
@@ -32,7 +37,14 @@ namespace Task1
             }
             else
                 Start(input);
+
+
+
+
+
         }
+
+        
 
         static async void Start(string startFolder = @"F:\test")
         {  
@@ -61,7 +73,86 @@ namespace Task1
             Console.WriteLine($"Grouping Copies --- DONE -- {sw.Elapsed}");
 
 
-            resultL.ForEach(g =>
+            Print(resultL);
+
+            sw.Stop();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("Ex-Time: " + sw.Elapsed);
+            IsDone = true;
+        }
+
+
+        /// <summary>
+        /// test 2
+        /// </summary>
+        /// <param name="startFolder"></param>
+        static void Start2(string startFolder = @"F:\Фото")
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
+            IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories).ToList();
+
+            //  fileList = fileList.Take(2000);
+            Console.WriteLine("Files Count: " + fileList.Count());
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var result = fileList.AsParallel().GroupBy(el => el.Length).ToList();
+            //  var result = await Task.WhenAll(task);
+
+
+            var tasks = new List<Task>();
+
+            result.ForEach(g =>
+            {
+
+                if (g.Count() > 1)
+                {
+
+                    if (tasks.Count() > 4)
+                    {
+                        Task.WaitAny(tasks.ToArray());
+                    }
+
+                    var ntasks = Task.Factory.StartNew(() =>
+                    {
+                        var fG = g.GroupBy(el => BitConverter.ToString(File.ReadAllBytes(el.FullName))).ToList();
+
+                        fG.ForEach(g2 =>
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"--Original File {g2.First().FullName} --- {g2.Count() - 1} Copies");
+                        });
+                    });
+
+                    tasks.Add(ntasks);
+                }
+            });
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine($"Getting Hash --- DONE -- {sw.Elapsed}");
+
+            //var resultL = result.ToList().GroupBy(el => GetHash(el.Hash)).ToList();
+            //Console.WriteLine($"Grouping Copies --- DONE -- {sw.Elapsed}");
+
+
+            // Print(resultL);
+
+            Task.WaitAll(tasks.ToArray());
+
+            sw.Stop();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("Ex-Time: " + sw.Elapsed);
+           // IsDone = true;
+        }
+
+
+        public static void Print(List<IGrouping<string, CompareItem>> list)
+        {
+            list.ForEach(g =>
             {
                 var groupe = g.ToList();
                 var head = groupe.FirstOrDefault();
@@ -71,16 +162,6 @@ namespace Task1
                 Console.WriteLine($"--- {groupe.Count - 1} Copies");
 
             });
-
-
-
-            sw.Stop();
-
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine();
-            Console.WriteLine("Ex-Time: " + sw.Elapsed);
-
-            IsDone = true;
         }
 
 
@@ -89,31 +170,6 @@ namespace Task1
         {
             return BitConverter.ToString(File.ReadAllBytes(path));
         }
-
-        //public static string GetFileHash(string fileName)
-        //{
-        //    string fileHash = string.Empty;
-
-        //    try
-        //    {
-        //        using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-        //        {
-        //            fileHash = Encoding.UTF8.GetString(new SHA1Managed().ComputeHash(fileStream));
-        //        }
-        //    }
-        //    catch (FileNotFoundException ex)
-        //    {
-        //        // Handle your exceptions
-        //        ex.ToString();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Handle your exceptions
-        //        ex.ToString();
-        //    }
-
-        //    return fileHash;
-        //}
 
     }
 
